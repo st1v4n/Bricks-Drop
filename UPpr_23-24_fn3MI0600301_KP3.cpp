57968fe54d5ee5031f8b81743c724bf1bc5f7538
st1v4n <149948320+st1v4n@users.noreply.github.com>
2024-01-06 12:26:57 +0200

    Add loginToAccount function
    added login to account function + fixed some minor issues with previous code.

--- UPpr_23-24_fn3MI0600301_KP3.cpp
+++ UPpr_23-24_fn3MI0600301_KP3.cpp
@@ -19,8 +19,8 @@
 #include <ctime>
 //that symbol will be to visualize the game field (it will form the border)
 const char BorderSymbol = '@';
-//the field x will be 10 + 2 at start, because we will include the border, that consists of 1 symbol on the left and 1 symbol on the right
-constexpr int FieldX = 16 + 2;
+//the field x will be 10 + 2 at start, because we will include the border, that consists of 1 symbol on the left and 1 symbol on the right + '\0' at the end of every row
+constexpr int FieldX = 16 + 3;
 // the same reason for field y, but now 1 symbol on top and 1 symbol down
 constexpr int FieldY = 16 + 2;
 constexpr size_t MAX_STRING_SIZE = 128;
@@ -30,17 +30,19 @@ char password[MAX_STRING_SIZE];
 int score = 0;
 int maxScore = 0;
 int getRandomNumber(int intervalLen, int startPoint) {
-    srand(time(NULL));
     int randomNumber = rand() % intervalLen + startPoint;
     return randomNumber;
 }
+int convertCharToInt(char symbol) {
+    return symbol - '0';
+}
 void generateGameField(char field[][FieldX], char emptySymbol) {
-    for (int i = 0;i < FieldX;i++) {
+    for (int i = 0;i < FieldX-1;i++) {
         field[0][i] = BorderSymbol;
     }
     for (int y = 1;y < FieldY-1;y++) {
-        for (int x = 0;x < FieldX;x++) {
-            if (x == 0 || x == FieldX - 1) {
+        for (int x = 0;x < FieldX-1;x++) {
+            if (x == 0 || x == FieldX - 2) {
                 field[y][x] = BorderSymbol;
             }
             else {
@@ -48,13 +50,16 @@ void generateGameField(char field[][FieldX], char emptySymbol) {
             }
         }
     }
-    for (int i = 0;i < FieldX;i++) {
+    for (int i = 0;i < FieldX-1;i++) {
         field[FieldY-1][i] = BorderSymbol;
+    }
+    for (int j = 0;j < FieldY - 1;j++) {
+        field[j][FieldX - 1] = '\0';
     }
 }
 void printField(const char field[][FieldX]) {
     for (int y = 0;y < FieldY;y++) {
-        for (int x = 0;x < FieldX;x++) {
+        for (int x = 0;x < FieldX-1;x++) {
             std::cout << field[y][x];
         }
         std::cout << std::endl;
@@ -68,6 +73,15 @@ int StringLengthFunc(const char* str) {
         len++;
     }
     return len;
+}
+int convertToInt(const char* str) {
+    if (!str)return -1;
+    int result = 0;
+    while (*str) {
+        (result *= 10) += convertCharToInt(*str);
+        str++;
+    }
+    return result;
 }
 void writeDataToFile(const char* username,const char* password, int score) {
     if (!username || !password) return;
@@ -83,9 +97,60 @@ void writeDataToFile(const char* username,const char* password, int score) {
     ofs.clear();
     ofs.close();
 }
+bool compareStrings(const char* str1, const char* str2) {
+    if (!str1 || !str2)return false;
+    while (*str1 && *str2) {
+        if ((*str1) != (*str2))return false;
+        str1++;
+        str2++;
+    }
+    return (*str1) == (*str2);
+}
+// no need to type username and password as parameters of the function because they are globally declared
+void loginToAccount() {
+    if (!username || !password) return;
+    std::ifstream ifs("data.txt");
+    if (!ifs.is_open()) {
+        std::cout << "Unexpected Error! You will play as guest!";
+    }
+    char previousMaxScore[MAX_STRING_SIZE];
+    char previousUsername[MAX_STRING_SIZE];
+    char previousPassword[MAX_STRING_SIZE];
+    ifs.getline(previousMaxScore, MAX_STRING_SIZE);
+    ifs.getline(previousUsername, MAX_STRING_SIZE);
+    ifs.getline(previousPassword, MAX_STRING_SIZE);
+    maxScore = convertToInt(previousMaxScore);
+    while (!compareStrings(username, previousUsername) || !compareStrings(password, previousPassword)) {
+        std::cout << "Username or Password is invalid! Enter new ones: \n";
+        std::cout << "Username: ";
+        std::cin >> username;
+        std::cout << "Password: ";
+        std::cin >> password;
+    }
+    ifs.clear();
+    ifs.close();
+}
+// no need to type username and password as parameters of the function because they are globally declared
+void enterAccountInfo() {
+    if (!username || !password)return;
+    std::cout << "Enter Username: ";
+    std::cin >> username;
+    while (StringLengthFunc(username) < minimumSymbols) {
+        std::cout << "Your username is too short, enter new one: ";
+        std::cin >> username;
+    }
+    std::cout << "Enter Password: ";
+    std::cin >> password;
+    while (StringLengthFunc(password) < minimumSymbols) {
+        std::cout << "Your password is too short, enter new one: ";
+        std::cin >> password;
+    }
+}
 int main()
 {
-    char field[FieldX][FieldY];
+    char field[FieldY][FieldX];
+    generateGameField(field, ' ');
+    srand(time(NULL));
     std::cout << "Welcome to Bricks Drop!"<<std::endl;
     std::cout << "Enter the number of the command you want to execute: "<<std::endl;
     std::cout << "Currently available commands: " << std::endl;
@@ -95,24 +160,19 @@ int main()
     int command;
     do {
         std::cin >> command;
-    } while (command < 1 || command>3);
-    if (command == 1) {
-        std::cout << "Enter Username: ";
-        std::cin >> username;
-        while (StringLengthFunc(username) < minimumSymbols) {
-            std::cout << "Your username is too short, enter new one: ";
-            std::cin >> username;
-        }
-        std::cout << "Enter Password: ";
-        std::cin >> password;
-        while (StringLengthFunc(password) < minimumSymbols) {
-            std::cout << "Your password is too short, enter new one: ";
-            std::cin >> password;
-        }
+    } while (command < 1 || command > 2);
+    switch (command) {
+    case 1: 
+        enterAccountInfo();
         writeDataToFile(username, password, score);
-    }
-    else if (command == 2) {
-        // currently under development, check out later for updates :)
+
+        break;
+    case 2:
+        enterAccountInfo();
+        loginToAccount();
+
+        break;
+    default: std::cout << "ERROR!";break;
     }
 }
 
